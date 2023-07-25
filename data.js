a1b767102c5478e1bae4b783d393b0cb630449d7
xtadeushx <alekscherkasov13@gmail.com>
2023-07-25 12:11:30 +0300

    feat: add mock data

---
+++ data.js
@@ -0,0 +1,26 @@
+const DATA = [
+  {
+    src: './assets/shopping-cart.png',
+    title: 'Shopping list',
+    createdAt: 'April,20 2021',
+    category: 'task',
+    content: ['tomatoes', 'bread'],
+    dates: [],
+  },
+  {
+    src: './assets/thought.png',
+    title: 'The theory of evolution',
+    createdAt: 'April,27 2021',
+    category: 'Random thoughts',
+    content: ['The theory of evolution'],
+    dates: [],
+  },
+  {
+    src: './assets/lightbulb.png',
+    title: 'New feature',
+    createdAt: 'April,27 2021',
+    category: 'Idea',
+    content: ['Implement new feature'],
+    dates: ['3/5/2021', '5/5/2021'],
+  },
+];
