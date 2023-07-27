86e7e3dfdfdf26258c08c738000126025bb7f312
xtadeushx <alekscherkasov13@gmail.com>
2023-07-27 09:57:11 +0300

    feat: add mock data

--- data.js
+++ data.js
@@ -26,6 +26,44 @@ const DATA = [
     content: ['Implement new feature'],
     dates: ['3/5/2021', '5/5/2021'],
   },
+
+  {
+    id: 4,
+    src: './assets/shopping-cart.png',
+    title: 'Learning list',
+    createdAt: 'April,21 2021',
+    category: 'task',
+    content: ['React', 'Redux'],
+    dates: [],
+  },
+  {
+    id: 5,
+    src: './assets/quote.png',
+    title: 'Steve Jobs',
+    createdAt: 'October,21 2019',
+    category: 'quote',
+    content: ['We are here to contribute to this world. Why else are we here?'],
+    dates: [],
+  },
+  
+  {
+    id: 6,
+    src: './assets/lightbulb.png',
+    title: 'New feature',
+    createdAt: 'June,27 2021',
+    category: 'Idea',
+    content: ['Fix some bugs'],
+    dates: [],
+  },
+  {
+    id: 7,
+    src: './assets/quote.png',
+    title: 'Albert Einstein',
+    createdAt: 'January,14 2020',
+    category: 'quote',
+    content: ["The smarter a person is, the easier he recognizes himself as a fool." ],
+    dates: [],
+  },
 ];
 
 export { DATA };
