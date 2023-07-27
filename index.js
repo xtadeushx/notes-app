e65c8999c978457cb60847463dc9e9689cb51c54
xtadeushx <alekscherkasov13@gmail.com>
2023-07-27 11:26:03 +0300

    feat: implement renderRow func

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { DATA } from './data.js';
 import { formatDateLong, formatDateShort } from './helpers/dateHelper.js';
 import { createElement } from './helpers/domHelper.js';
+import { renderRow } from './renderRow.js';
 
 const CLASSES = {
   MODAL_ACTIVE: 'modal-active',
@@ -42,11 +43,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function render(data, root) {
   root.innerHTML = '';
   data.map((el) => {
-    const item = createElement({
-      tagName: 'li',
-      className: 'table__item item',
-    });
-    item.id = el.id;
+    const item = renderRow(el);
+
     item.addEventListener('click', (event) => {
       let key = event.target?.closest('button')?.getAttribute('data-set');
       switch (key) {
@@ -64,24 +62,7 @@ function render(data, root) {
           break;
       }
     });
-    item.innerHTML = `
-    <div class="item__img-container">
-      <span class="item__img-wrapper"><img class="item__img button--light" src=${
-        el.src
-      }
-          alt=${el.title}></span>
-      <p class="item__title">${el.title}</p>
-    </div>
-    <p class="item__createdAt">${el.createdAt}</p>
-    <p class="item__category">${el.category}</p>
-    <p class="item__content">${el.content.join(', ')}</p>
-    <p class="item__dates">${el.dates.join(', ')}</p>
-    <div class="item__buttons buttons__wrapper">
-      <button class="button edit" data-set='edit'><img src="./assets/editing.png" alt="editing button"></button>
-      <button class="button archive" data-set='archive'><img src="./assets/archive.png" alt="archive button"></button>
-      <button class="button delete" data-set='delete'><img src="./assets/delete.png" alt="delete button"></button>
-    </div>`;
-
+    
     root.appendChild(item);
   });
 };
